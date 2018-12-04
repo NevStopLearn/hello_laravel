a5648b3a41b93b37d98f9bca37c0105f5a9e894f
JiYang <2574714005@qq.com>
2018-12-04 18:18:19 +0800

    user-curd half finished

--- app/Http/Controllers/Home/UserController.php
+++ app/Http/Controllers/Home/UserController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Home;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except'    => ['show','create','store']
+        ]);
+    }
 
     public function show(User $user)
     {
@@ -37,5 +44,33 @@ class UserController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('user.show',[$user]);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request)
+    {
+        $this->validate($request,[
+            //'name'      => 'required|max:50|unique:users,name,'.$user->id,
+            'name'      => ['required','max:50',Rule::unique('users')->ignore($user->id)],
+            'password'  => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success','个人资料更改成功！');
+
+        return redirect()->route('user.show',$user->id);
     }
 }
--- resources/views/layout/_header.blade.php
+++ resources/views/layout/_header.blade.php
@@ -12,7 +12,7 @@
                             </a>
                             <ul class="dropdown-menu">
                                 <li><a href="{{ route('user.show', Auth::user()->id) }}">个人中心</a></li>
-                                <li><a href="#">编辑资料</a></li>
+                                <li><a href="{{ route('user.edit', Auth::user()->id) }}">编辑资料</a></li>
                                 <li class="divider"></li>
                                 <li>
                                     <a id="logout" href="#">
