2d7d90591c77c448ac49efd6fc8a578c154d9239
JiYang <2574714005@qq.com>
2018-12-06 18:12:32 +0800

    statuses add half

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Carbon::setLocale('zh');
         Schema::defaultStringLength(191);
     }
 
